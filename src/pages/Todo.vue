9e611de4875ac13aee71f5edbd4c14409850dd51
krafik <alexeym4l@mail.ru>
2023-08-17 13:16:47 +0300

    статический список, обработчик кликов, удаление из списка задач

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,60 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+
+
+    <q-list separator bordered>
+
+      <q-item @click="task.done = !task.done" clickable :class="{'done bg-green-2' : task.done}" v-for="(task, index) in tasks" :key="task.title" v-ripple>
+        <q-item-section  avatar>
+          <q-checkbox class="no-pointer-events" v-model="task.done" val="teal" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn @click.stop="deleteTask(index)" dense flat round color="primary" icon="delete" />
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+
+
   </q-page>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'Hello',
+          done: true
+        },
+        {
+          title: 'Hello 2',
+          done: false
+        },
+        {
+          title: 'Hello 3',
+          done: false
+        }
+      ]
+    }
+  },
+  methods:{
+    deleteTask(index){
+      this.tasks.splice(index,1)
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+.done{
+  .q-item__label{
+    text-decoration: line-through;
+    color: grey;
+  }
+}
+</style>
