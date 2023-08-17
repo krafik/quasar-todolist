c1eb6ab54744e82b928e8f9851b1044bb465003b
krafik <alexeym4l@mail.ru>
2023-08-17 14:57:04 +0300

    добавил диалоговое окно, поле ввода для новіх задач и логику работы

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,11 +1,23 @@
 <template>
   <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
 
+      <q-input @keyup.enter="addTask" filled class="col" square placeholder="add some task" bg-color="white"
+        v-model="newTask" label="Label" dense>
+
+
+        <template v-slot:append>
+          <q-btn @click="addTask" round dense flat icon="add" />
+        </template>
+      </q-input>
+
+    </div>
 
     <q-list separator bordered>
 
-      <q-item @click="task.done = !task.done" clickable :class="{'done bg-green-2' : task.done}" v-for="(task, index) in tasks" :key="task.title" v-ripple>
-        <q-item-section  avatar>
+      <q-item @click="task.done = !task.done" clickable :class="{ 'done bg-green-2': task.done }"
+        v-for="(task, index) in tasks" :key="task.title" v-ripple>
+        <q-item-section avatar>
           <q-checkbox class="no-pointer-events" v-model="task.done" val="teal" color="primary" />
         </q-item-section>
         <q-item-section>
@@ -26,6 +38,7 @@
 export default {
   data() {
     return {
+      newTask: '',
       tasks: [
         {
           title: 'Hello',
@@ -42,17 +55,41 @@ export default {
       ]
     }
   },
-  methods:{
-    deleteTask(index){
-      this.tasks.splice(index,1)
+  methods: {
+    deleteTask(index) {
+
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'do u wanna realy delete?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index, 1);
+        this.$q.notify('delete!')
+      }).onOk(() => {
+        // console.log('>>>> second OK catcher')
+      }).onCancel(() => {
+        // console.log('>>>> Cancel')
+      }).onDismiss(() => {
+        // console.log('I am triggered on both OK and Cancel')
+      })
+
+
+    },
+    addTask() {
+      if (this.newTask != '') {
+         this.tasks.push({ title: this.newTask, done: false });
+          this.newTask = '';
+         } else{
+         this.$q.notify('input is empty!')}
     }
   }
 }
 </script>
 
 <style lang="scss">
-.done{
-  .q-item__label{
+.done {
+  .q-item__label {
     text-decoration: line-through;
     color: grey;
   }
